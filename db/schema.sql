0024101b88dba4bfc204ce79368d7c57d6de3fa4
tom <thomas_polczyk@hotmail.com>
2025-11-15 14:00:34 +0000

    Updated Scheme for better compatibility with tech app

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,29 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE TABLE `communications` (
+  `CommunicationID` int(11) NOT NULL AUTO_INCREMENT,
+  `JobID` int(11) DEFAULT NULL,
+  `CommunicationType` varchar(50) DEFAULT NULL,
+  `DateTime` datetime DEFAULT current_timestamp(),
+  `Note` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`CommunicationID`),
+  KEY `JobID` (`JobID`),
+  CONSTRAINT `communications_ibfk_1` FOREIGN KEY (`JobID`) REFERENCES `jobs` (`JobID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+
+CREATE TABLE `costs` (
+  `CostID` int(11) NOT NULL AUTO_INCREMENT,
+  `JobID` int(11) DEFAULT NULL,
+  `CostType` varchar(50) DEFAULT NULL,
+  `Amount` decimal(10,2) DEFAULT NULL,
+  `Description` text DEFAULT NULL,
+  PRIMARY KEY (`CostID`),
+  KEY `JobID` (`JobID`),
+  CONSTRAINT `costs_ibfk_1` FOREIGN KEY (`JobID`) REFERENCES `jobs` (`JobID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+
 CREATE TABLE `customers` (
   `CustomerID` int(11) NOT NULL AUTO_INCREMENT,
   `FirstName` varchar(50) DEFAULT NULL,
@@ -33,6 +57,39 @@ CREATE TABLE `jobs` (
   CONSTRAINT `jobs_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+
+CREATE TABLE `orders` (
+  `PartID` int(11) NOT NULL AUTO_INCREMENT,
+  `JobID` int(11) DEFAULT NULL,
+  `OrderDate` date DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Quantity` int(11) DEFAULT NULL,
+  `TotalCost` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`PartID`),
+  KEY `JobID` (`JobID`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`JobID`) REFERENCES `jobs` (`JobID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+
+CREATE TABLE `payments` (
+  `PaymentID` int(11) NOT NULL AUTO_INCREMENT,
+  `JobID` int(11) DEFAULT NULL,
+  `Date` DATE DEFAULT NULL,
+  `Amount` decimal(10,2) DEFAULT NULL,
+  `PaymentType` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`PaymentID`),
+  KEY `JobID` (`JobID`),
+  CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`JobID`) REFERENCES `jobs` (`JobID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+
+CREATE TABLE `walkins` (
+  `WalkinID` int(11) NOT NULL AUTO_INCREMENT,
+  `WalkinDate` date DEFAULT current_timestamp(),
+  `Amount` int(11) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`WalkinID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 CREATE TABLE `HowHeard` (
   `JobID` int(11) NOT NULL,
